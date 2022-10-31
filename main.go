39aef2a3b0fcb19fa676779001944af53fbde4cd
higor-haider <higor.haider@sumup.com>
2022-10-31 20:44:50 -0300

    Working with go routines and channels

--- main.go
+++ main.go
@@ -14,17 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "migh be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
